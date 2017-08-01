b8b7ff65673cabde6e59b49d40b60fc152f6f065
Kip Gibson <Kiptong@gmail.com>
2017-08-01 15:45:46 -0700

    User can view a list of notes. #1

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
-import Notes from 'components/notes-list'
+import Notes from './components/notes-list'
 
 ReactDOM.render(
   <Notes />,
-  document.getElementByID('root')
+  document.getElementById('root')
 )
